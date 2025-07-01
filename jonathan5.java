d75c79e82069774510fe8c8671c12bc47fad0b7f
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-07-01 07:55:20 -0300

    Update jonathan5.java

--- jonathan5.java
+++ jonathan5.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
 
+public class jonathan5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int soma = 0 ;
+
+        for (int     n = 1; n <10; n++){
+            soma += n;
+            double media = (double) soma / 10;
+            System.out.println("A soma dos 10 primeiros numeros é: "+ n + " + " + (n+1) + " = " + soma);
+            System.out.println("A media é : "+ media);
+        }
+        sc.close();
+        }
+
+    }//nao acabei
