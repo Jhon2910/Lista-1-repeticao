e3ce439a070f752910bdeef841ee58c8e55b1a0c
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-07-08 17:39:17 -0300

    Update jonathan6.java

--- jonathan6.java
+++ jonathan6.java
@@ -1,18 +1,24 @@
 import java.util.Scanner;
 
-public class jonathan5 {
+public class jonathan6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Digite a quantidade de n primeiros numeros: ");
-        int soma = 0;
+        System.out.print("Digite o numero final : ");
 
-        for (int n = sc.nextInt();n > 0;n++){
-            soma+=1;
-            double media = (double) soma/n;
-            System.out.println("A soma é: "+n+" + "+n+" = "+ soma);
-            System.out.println("A media é: "+ media);
+        int num = sc.nextInt();
+        int soma = 0;
+        int n = 1;
+
+        while (n <= num){
+            soma = soma + n;
+            n++;
         }
+
+        double media = (double) soma /num;
+
+        System.out.println("A soma de "+ num +" primeiros positivos e: "+soma);
+        System.out.println("A media de " +num+ " primeiros numeros positivos e: "+ media);
         sc.close();
-        }
-    }//nao terminei
+    }
+}
